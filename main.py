625f506c7569d2363b47c56c6c00eae4ede5f6c5
Maishasan <hsnmys97@gmail.com>
2025-01-31 13:45:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ clean = median(lena, ball(3))
 l_edge = edge_detection(clean)
 binary = l_edge > 100
 edge_image = Image.fromarray(np.uint8(binary * 255))
-edge_image.save('my_edges.png')
+edge_image.save('Lenanew.png')
